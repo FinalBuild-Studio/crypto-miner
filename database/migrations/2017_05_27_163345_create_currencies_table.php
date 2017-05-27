1f63fc484114857e6069d2a9dc2d5eedeae3dbdf
michael34435 <michael34435@capslock.tw>
2017-05-28 00:50:21 +0800

    Add migrations

---
+++ database/migrations/2017_05_27_163345_create_currencies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('type');
+            $table->float('amount')->default(0);
+            $table->tinyInteger('status')->default(false);
+            $table->tinyInteger('event')->nullable(false);
+            $table->integer('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
---
+++ database/migrations/2017_05_27_164601_create_investments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvestmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('investments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('amount')->default(0);
+            $table->timestamp('expired_at')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('investments');
+    }
+}
