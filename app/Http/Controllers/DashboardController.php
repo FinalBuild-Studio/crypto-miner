53000011be88b48202a8a71cd7df9d930e06cda2
michael34435 <michael34435@capslock.tw>
2017-06-02 23:33:45 +0800

    Add dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\{Investment, Revenue, Currency};
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -9,6 +10,41 @@ class DashboardController extends Controller
 
     public function index()
     {
+        $user = request()->user();
+
+        /**
+         * get current user percentage
+         */
+        $percentages = Investment::percentage();
+        $percentage  = $percentages[$user->id] * 100;
+
+        /**
+         * ETH
+         */
+        $eth       = Revenue::user($user->id, Currency::ETH);
+        $ethWallet = Wallet::user($user->id, Currency::ETH);
+
+        /**
+         * BTC
+         */
+        $btc       = Revenue::user($user->id, Currency::BTC);
+        $btcWallet = Wallet::user($user->id, Currency::BTC);
+
+        /**
+         * TWD
+         */
+        $twdWallet = Wallet::user($user->id, Currency::TWD);
+
+        /**
+         * set view variable
+         */
+        view()->share('percentage', $percentage);
+        view()->share('eth', $eth);
+        view()->share('ethWallet', $ethWallet);
+        view()->share('btc', $btc);
+        view()->share('btcWallet', $btcWallet);
+        view()->share('twd', $twdWallet);
+
         return view('dashboard');
     }
 }
