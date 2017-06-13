94df7ad61d69a3c345a07b52ce7481d8a8f54c3e
michael34435 <michael34435@capslock.tw>
2017-06-14 01:35:09 +0800

    Add title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title></title>
+    <title>登入</title>
     <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
     <meta name="viewport" content="width=device-width">
     <link href="{{ url('/css/app.css') }}" rel="stylesheet">
