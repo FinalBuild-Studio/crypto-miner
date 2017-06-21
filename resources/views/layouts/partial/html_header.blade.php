1b97cc7ae4b6ba80e3be20ae92e201feffbb58e1
michael34435 <michael34435@capslock.tw>
2017-06-21 18:54:15 +0800

    Add slash to html tag

--- resources/views/layouts/partial/html_header.blade.php
+++ resources/views/layouts/partial/html_header.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8" />
-<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="csrf-token" content="{{ csrf_token() }}" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
 <meta name="viewport" content="width=device-width" />
 <title>BTC/ETH Miner Management System</title>
-<link rel="stylesheet" href="{{ url('/css/app.css') }}">
+<link rel="stylesheet" href="{{ url('/css/app.css') }}" />
