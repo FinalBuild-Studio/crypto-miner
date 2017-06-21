fe8023baeb1e58197a555f2ea1941672c7a19062
michael34435 <michael34435@capslock.tw>
2017-06-21 16:54:57 +0800

    Fix withdraw function

--- app/Http/Controllers/Panel/Admin/Api/WithdrawController.php
+++ app/Http/Controllers/Panel/Admin/Api/WithdrawController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Panel\Admin\Api;
 
 use DB;
 use App\Exceptions\GeneralException;
-use App\{Currency, Investment, Revenue, Reason, Wallet};
+use App\{Currency, Investment, Revenue, Reason, Wallet, WithdrawLog};
 use App\Http\Controllers\Controller;
 
 class WithdrawController extends Controller
@@ -12,8 +12,7 @@ class WithdrawController extends Controller
 
     public function store()
     {
-        $json     = request()->json();
-        $currency = $json->get('currency');
+        $currency = request()->json()->get('currency');
         $currency = Currency::name($currency)->firstOrFail();
 
         if (!$currency->is_crypto) {
@@ -21,23 +20,41 @@ class WithdrawController extends Controller
         }
 
         $transfered = false;
-        DB::transaction(function() use ($currency, $amount, &$transfered) {
-            $percentages = investors($currency->id);
-            foreach ($percentages as $userId => $percentage) {
-                Revenue::createOrFail([
-                    'currency_id' => $currency->id,
-                    'amount'      => - ($percentage * $amount),
-                    'user_id'     => $userId,
-                    'reason_id'   => Reason::TRANSFER,
-                    'percentage'  => $percentage,
-                ]);
+        DB::transaction(function() use ($currency, &$transfered) {
+            $deposit  = [];
+            $amounts  = [];
+            $revenues = Revenue::currencyType($currency->id)->get();
+            foreach ($revenues as $revenue) {
+                $amounts[$currency->id] = $amount[$currency->id] ?? [];
+                $amounts[$currency->id][$revenue->user_id] = $amount[$currency->id][$revenue->user_id] ?? [];
+                $amounts[$currency->id][$revenue->user_id] = $revenue;
 
-                Wallet::createOrFail([
-                    'currency_id' => $currency->id,
-                    'amount'      => $percentage * ($amount - $currency->fee),
-                    'user_id'     => $userId,
-                    'percentage'  => $percentage,
-                ]);
+                $deposit[$currency->id]  = $deposit[$currency->id] ?? 0;
+                $deposit[$currency->id] += $revenue->amount;
+            }
+
+            foreach ($amounts as $currencyId => $amount) {
+                foreach ($amount as $userId => $revenue) {
+                    $amountSum = collect($revenue)->sum('amount');
+                    $percentage = $sum / $deposit[$currencyId];
+
+                    if ($sum > 0) {
+                        Revenue::createOrFail([
+                            'currency_id' => $currencyId,
+                            'amount'      => - $amountSum,
+                            'user_id'     => $userId,
+                            'reason_id'   => Reason::TRANSFER,
+                            'percentage'  => $percentage,
+                        ]);
+
+                        Wallet::createOrFail([
+                            'currency_id' => $currencyId,
+                            'amount'      => $amountSum - $percentage * $currency->fee,
+                            'user_id'     => $userId,
+                            'percentage'  => $percentage,
+                        ]);
+                    }
+                }
             }
 
             $transfered = true;
