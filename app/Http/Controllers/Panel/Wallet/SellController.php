0ab181c44b688f5f3bff5abfa7131a0759d8ffc7
michael34435 <michael34435@capslock.tw>
2017-06-23 14:39:52 +0800

    Fix SellController

--- app/Http/Controllers/Panel/Wallet/SellController.php
+++ app/Http/Controllers/Panel/Wallet/SellController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Panel\Wallet;
 
 use DB;
 use App\Exceptions\GeneralException;
-use App\{Wallet, Currency};
+use App\{Currency, Wallet};
 use App\Http\Controllers\Controller;
 
 class SellController extends Controller
@@ -15,32 +15,33 @@ class SellController extends Controller
         $user     = request()->user();
         $currency = request()->input('currency');
         $amount   = request()->input('amount', 0);
-        $priceAt  = request()->input('price_at', 0);
-        $currency = Currency::name($currency);
-
-        $wallet = Wallet::user($user->id, $currency->id);
+        $priceAt  = request()->input('price_at', -1);
+        $currency = Currency::name($currency)->firstOrFail();
 
         if (!$currency->is_crypto) {
             throw new GeneralException(100);
-        }
-
-        if ($amount > $wallet) {
-            throw new GeneralException(101);
-
         }
 
         if ($currency->min_sell > $amount) {
             throw new GeneralException(102);
         }
 
+        $wallet = Wallet::user($user->id)
+            ->currencyType($currency->id)
+            ->sum('amount');
+
+        if ($amount > $wallet) {
+            throw new GeneralException(101);
+        }
+
         DB::transaction(function() use ($amount, $currency, $user, $priceAt) {
-            Wallet::createOrFail([
+            Wallet::create([
                 'amount'      => - $amount,
                 'currency_id' => $currency->id,
                 'user_id'     => $user->id,
             ]);
 
-            Transfer::createOrFail([
+            Transfer::create([
                 'amount'      => $amount,
                 'currency_id' => $currency->id,
                 'user_id'     => $user->id,
