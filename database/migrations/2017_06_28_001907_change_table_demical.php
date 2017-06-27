98b2a112840bb1928da2600e1797b4dbf3bc626f
michael34435 <michael34435@capslock.tw>
2017-06-28 00:20:18 +0800

    Add migration

---
+++ database/migrations/2017_06_28_001907_change_table_demical.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeTableDemical extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transfers', function (Blueprint $table) {
+            $table->decimal('amount', 20, 12)->change();
+        });
+
+        Schema::table('logs', function (Blueprint $table) {
+            $table->decimal('amount', 20, 12)->change();
+        });
+
+        Schema::table('revenues', function (Blueprint $table) {
+            $table->decimal('amount', 20, 12)->change();
+        });
+
+        Schema::table('investments', function (Blueprint $table) {
+            $table->decimal('amount', 20, 12)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transfers', function (Blueprint $table) {
+            $table->decimal('amount', 15, 12)->change();
+        });
+
+        Schema::table('logs', function (Blueprint $table) {
+            $table->decimal('amount', 15, 12)->change();
+        });
+
+        Schema::table('revenues', function (Blueprint $table) {
+            $table->decimal('amount', 15, 12)->change();
+        });
+
+        Schema::table('investments', function (Blueprint $table) {
+            $table->decimal('amount', 15, 12)->change();
+        });
+    }
+}
