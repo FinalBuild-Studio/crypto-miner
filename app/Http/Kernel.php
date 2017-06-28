a10ae0e9a10457880cb14671907fdb9f3b1dc619
michael34435 <michael34435@capslock.tw>
2017-06-28 10:34:07 +0800

    Try to add session to http kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -27,15 +27,23 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middlewareGroups = [
-        'web' => [
+        'sessions' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        ],
+
+        'views' => [
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\AppendURIToView::class,
+        ],
+
+        'web' => [
+            'sessions',
+            'views',
         ],
 
         'api' => [
@@ -55,6 +63,7 @@ class Kernel extends HttpKernel
         ],
 
         'panel.admin.api' => [
+            'sessions',
             'auth',
             'admin',
             'api',
