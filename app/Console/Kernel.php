95f19a2502561afd37a0757167a7cc15c4d4d4a1
michael34435 <michael34435@capslock.tw>
2017-07-03 18:28:11 +0800

    Add transfer:update to task command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('investment:update')->hourly();
+        $schedule->command('transfer:update')->quarterly();
     }
 
     /**
