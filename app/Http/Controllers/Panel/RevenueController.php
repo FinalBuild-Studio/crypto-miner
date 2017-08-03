0e3ff347fca5a52d8d120b814dbc19f31b88d2b0
michael34435 <michael34435@capslock.tw>
2017-08-03 16:11:57 +0800

    Add new constroller function for send cryptocoin to user

--- app/Http/Controllers/Panel/RevenueController.php
+++ app/Http/Controllers/Panel/RevenueController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Panel;
 
-use App\Revenue;
+use DB;
+use App\{Revenue, User, Reason};
+use App\Exceptions\GeneralException;
 use App\Http\Controllers\Controller;
 
 class RevenueController extends Controller
@@ -16,5 +18,39 @@ class RevenueController extends Controller
         view()->share('revenues', $revenues);
 
         return view('panel.revenue');
+    }
+
+    public function store()
+    {
+        $user      = request()->user();
+        $recipient = request()->input('recipient');
+        $amount    = request()->input('amount');
+        $type      = request()->input('type');
+
+        $total = Revenue::user($user->id)->currencyType($type)->sum('amount');
+
+        if ($amount > $total || $amount < 0) {
+            throw new GeneralException(101);
+        }
+
+        $recipient = User::whereId($recipient)->firstOrFail();
+
+        DB::transaction(function() use ($user, $recipient, $type, $amount) {
+            Revenue::create([
+                'user_id'     => $user->id,
+                'amount'      => - $amount,
+                'currency_id' => $type,
+                'reason_id'   => Reason::SEND,
+            ]);
+
+            Reason::create([
+                'user_id'     => $recipient->id,
+                'amount'      => $amount,
+                'currency_id' => $type,
+                'reason_id'   => Reason::RECEIVE,
+            ]);
+        });
+
+        return back();
     }
 }
--- app/Reason.php
+++ app/Reason.php
@@ -10,4 +10,6 @@ class Reason extends Model
     public const REVENUE     = 1;
     public const TRANSFER    = 2;
     public const MAINTENANCE = 3;
+    public const SEND        = 4;
+    public const RECEIVE     = 5;
 }
